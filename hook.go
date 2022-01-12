73edb9146b2ddb6f632ff1d6868871fcf9f81a24
BitFlitsche <olliputzer@gmail.com>
2022-01-12 21:45:20 +0100

    testing loglevels

--- hook.go
+++ hook.go
@@ -5,11 +5,9 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/BitFlitsche/eslogrus/internal/bulk"
 	"github.com/elastic/go-elasticsearch/v7/esapi"
 	"net/http"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/elastic/go-elasticsearch/v7"
@@ -55,24 +53,6 @@ func NewElasticHook(client *elasticsearch.Client, host string, level logrus.Leve
 	return NewElasticHookWithFunc(client, host, level, func() string { return index })
 }
 
-// NewAsyncElasticHook creates new  hook with asynchronous log.
-// client - ElasticSearch client with specific es version (v5/v6/v7/...)
-// host - host of system
-// level - log level
-// index - name of the index in ElasticSearch
-func NewAsyncElasticHook(client *elasticsearch.Client, host string, level logrus.Level, index string) (*ElasticHook, error) {
-	return NewAsyncElasticHookWithFunc(client, host, level, func() string { return index })
-}
-
-// NewBulkProcessorElasticHook creates new hook that uses a bulk processor for indexing.
-// client - ElasticSearch client with specific es version (v5/v6/v7/...)
-// host - host of system
-// level - log level
-// index - name of the index in ElasticSearch
-func NewBulkProcessorElasticHook(client *elasticsearch.Client, host string, level logrus.Level, index string) (*ElasticHook, error) {
-	return NewBulkProcessorElasticHookWithFunc(client, host, level, func() string { return index })
-}
-
 // NewElasticHookWithFunc creates new hook with
 // function that provides the index name. This is useful if the index name is
 // somehow dynamic especially based on time.
@@ -84,39 +64,12 @@ func NewElasticHookWithFunc(client *elasticsearch.Client, host string, level log
 	return newHookFuncAndFireFunc(client, host, level, indexFunc, syncFireFunc)
 }
 
-// NewAsyncElasticHookWithFunc creates new asynchronous hook with
-// function that provides the index name. This is useful if the index name is
-// somehow dynamic especially based on time.
-// client - ElasticSearch client with specific es version (v5/v6/v7/...)
-// host - host of system
-// level - log level
-// indexFunc - function providing the name of index
-func NewAsyncElasticHookWithFunc(client *elasticsearch.Client, host string, level logrus.Level, indexFunc IndexNameFunc) (*ElasticHook, error) {
-	return newHookFuncAndFireFunc(client, host, level, indexFunc, asyncFireFunc)
-}
-
-// NewBulkProcessorElasticHookWithFunc creates new hook with
-// function that provides the index name. This is useful if the index name is
-// somehow dynamic especially based on time that uses a bulk processor for
-// indexing.
-// client - ElasticSearch client with specific es version (v5/v6/v7/...)
-// host - host of system
-// level - log level
-// indexFunc - function providing the name of index
-func NewBulkProcessorElasticHookWithFunc(client *elasticsearch.Client, host string, level logrus.Level, indexFunc IndexNameFunc) (*ElasticHook, error) {
-	fireFunc, err := makeBulkFireFunc(client)
-	if err != nil {
-		return nil, err
-	}
-	return newHookFuncAndFireFunc(client, host, level, indexFunc, fireFunc)
-}
-
 func newHookFuncAndFireFunc(client *elasticsearch.Client, host string, level logrus.Level, indexFunc IndexNameFunc, fireFunc fireFunc) (*ElasticHook, error) {
 	var levels []logrus.Level
 	for _, l := range []logrus.Level{
 		logrus.PanicLevel,
 		logrus.FatalLevel,
-		logrus.ErrorLevel,
+		// logrus.ErrorLevel,
 		logrus.WarnLevel,
 		logrus.InfoLevel,
 		logrus.DebugLevel,
@@ -161,13 +114,6 @@ func (hook *ElasticHook) Fire(entry *logrus.Entry) error {
 	return hook.fireFunc(entry, hook)
 }
 
-func asyncFireFunc(entry *logrus.Entry, hook *ElasticHook) error {
-	go func() {
-		_ = syncFireFunc(entry, hook) // TODO: how can we handle the error?
-	}()
-	return nil
-}
-
 func createMessage(entry *logrus.Entry, hook *ElasticHook) *message {
 	level := entry.Level.String()
 
@@ -204,71 +150,6 @@ func syncFireFunc(entry *logrus.Entry, hook *ElasticHook) error {
 	defer res.Body.Close()
 
 	return err
-}
-
-// Create closure with bulk processor tied to fireFunc.
-// Note: garbage collector will never be able to free memory allocated for bulkWriters
-func makeBulkFireFunc(client *elasticsearch.Client) (fireFunc, error) {
-	bulkWriters := make(map[*ElasticHook]*bulk.Writer)
-	var lock sync.RWMutex
-
-	getWriter := func(hook *ElasticHook) *bulk.Writer {
-		lock.RLock()
-		writer := bulkWriters[hook]
-		lock.RUnlock()
-		if writer != nil {
-			return writer
-		}
-		lock.Lock()
-		writer = bulkWriters[hook]
-		if writer != nil { // this is a second check to avoid sequential writes
-			lock.Unlock()
-			return writer
-		}
-
-		// long path, create a new writer
-		writer = bulk.NewBulkWriterWithErrorHandler(time.Second, func(data []byte) error {
-			res, err := client.Bulk(bytes.NewReader(data),
-				client.Bulk.WithIndex(hook.index()),
-			)
-			if err != nil {
-				return err
-			}
-			defer res.Body.Close()
-			if res.IsError() {
-				raw := make(map[string]interface{})
-				if err := json.NewDecoder(res.Body).Decode(&raw); err != nil {
-					return fmt.Errorf("failure to to parse response body: %s", err.Error())
-				} else {
-					return fmt.Errorf("error: [%d] %s: %s",
-						res.StatusCode,
-						raw["error"].(map[string]interface{})["type"],
-						raw["error"].(map[string]interface{})["reason"],
-					)
-				}
-				// A successful response might still contain errors for particular documents...
-				//
-			}
-			return nil
-		}, func(data []byte, err error) {
-			// TODO: how to handle the error??
-			// panic(fmt.Sprintf("error: %s", err))
-		})
-		bulkWriters[hook] = writer
-		lock.Unlock()
-
-		return writer
-	}
-
-	return func(entry *logrus.Entry, hook *ElasticHook) error {
-		data, err := json.Marshal(createMessage(entry, hook))
-		if err != nil {
-			return err
-		}
-		data = append([]byte(`{"index":{}}`+"\n"), data...)
-		_, _ = getWriter(hook).Write(append(data, '\n'))
-		return nil
-	}, nil
 }
 
 // Levels Required for logrus hook implementation
